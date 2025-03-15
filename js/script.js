de7a9409ed2f4daec7a3219ad1759938ac852513
Olivier <6237801@cstj.qc.ca>
2025-03-15 16:36:39 -0400

    modified:   css/_composant/header.css 	modified:   css/_composant/nav-menu.css 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log("hello world");
+// Variables et constantes du projet
+// - - - - - - - - - - - - - - - - - - - - - - - - - - -
+const panneauBtn = document.querySelector(".nav--btn");
+const panneauNav = document.querySelector(".nav--menu");
+const elementsPanneau = document.querySelectorAll(".nav--menu *")
+
+
+
+function OuvrirNav() {
+    panneauNav.style.opacity = 1;
+    panneauNav.style.width = "100%";
+    setTimeout(e=> {
+
+        for (let i=0; i < elementsPanneau.length; i++) {
+            let element = elementsPanneau[i];
+            element.style.opacity = 1;
+        }
+    },300);
+}
+
+function FermerNav() {
+    for (let i=0; i < elementsPanneau.length; i++) {
+        let element = elementsPanneau[i];
+        element.style.opacity = 0;
+    }
+    setTimeout(e=> {
+        panneauNav.style.opacity = 0;
+        panneauNav.style.width = "0%";
+    },300);
+}
